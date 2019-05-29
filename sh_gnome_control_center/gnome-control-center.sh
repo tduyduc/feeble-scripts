be5a603b6c567eeeefc149730413b9e1537e04f1
T.D. Stoneheart <tduyduc@ymail.com>
2019-05-29 18:03:55 +0700

    Create gnome-control-center.sh

---
+++ sh_gnome_control_center/gnome-control-center.sh
@@ -0,0 +1,2 @@
+# Launch GNOME Control Center, despite the use of any other desktop environment, which is useful for managing online accounts for Evolution
+XDG_CURRENT_DESKTOP=GNOME gnome-control-center 2> /dev/null
