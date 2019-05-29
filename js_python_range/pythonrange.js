f8624c99e080cb2834a0e76c83f502fe9146c6b7
T.D. Stoneheart <tduyduc@ymail.com>
2019-05-29 11:37:35 +0700

    Create pythonrange.js

---
+++ js_python_range/pythonrange.js
@@ -0,0 +1,14 @@
+function range(start, end = undefined, step = 1) {
+  step -= 0;
+  if (undefined === end) {
+    end = start;
+    start = 0;
+  }
+  for (var i = start - 0, len = end - 0, value = [],
+    continuing = (i, len) => {
+      if (!step) return false;
+      return (step > 0) ? (i < len) : (i > len);
+    }; continuing(i, len); i += step
+  ) value.push(i);
+  return value;
+}
