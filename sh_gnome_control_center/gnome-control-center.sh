fd8e4bbf574cfca68ce54b27e74601e03fef49cd
T.D. Stoneheart <tduyduc@ymail.com>
2019-06-05 11:42:25 +0700

    Update gnome-control-center.sh
    Add an ampersand for background running

--- sh_gnome_control_center/gnome-control-center.sh
+++ sh_gnome_control_center/gnome-control-center.sh
@@ -1,2 +1,2 @@
 # Launch GNOME Control Center, despite the use of any other desktop environment, which is useful for managing online accounts for Evolution
-XDG_CURRENT_DESKTOP=GNOME gnome-control-center 2> /dev/null
+XDG_CURRENT_DESKTOP=GNOME gnome-control-center 2> /dev/null &
