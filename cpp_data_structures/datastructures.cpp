7b9a56d2f3f04785535e1505ab5af9cd92ff2bde
T.D. Stoneheart <tduyduc@ymail.com>
2019-08-05 20:44:52 +0700

    Update indentation

--- cpp_data_structures/datastructures.cpp
+++ cpp_data_structures/datastructures.cpp
@@ -761,17 +761,6 @@ private:
 };
 
 template <typename E> class SQueue: public Queue<E> {
-	/*
-	enQueue(q,  x)
-	  1) Push x to stack1 (assuming size of stacks is unlimited).
-
-	deQueue(q)
-	  1) If both stacks are empty then error.
-	  2) If stack2 is empty
-	       While stack1 is not empty, push everything
-	       from stack1 to stack2.
-	  3) Pop the element from stack2 and return it.
-	*/
 private: LStack<E> s1, s2;
 public:
 	void clear() { s1.clear(); s2.clear(); }
