10e87e479be9a4577690af1bc577259993b88900
T.D. Stoneheart <tduyduc@ymail.com>
2020-01-07 18:52:37 +0700

    Create unlikepages.js

---
+++ js_facebook_unlike_pages/unlikepages.js
@@ -0,0 +1,30 @@
+// Unlike your liked Facebook pages
+
+// Before invoking this function, navigate to your likes list
+// The URL should be in the form: facebook.com/<username>/likes
+
+async function unlikePages() {
+  const mouseoverEvent = new Event('mouseover');
+  for (const element of document.getElementsByClassName('PageLikedButton')) {
+    element.dispatchEvent(mouseoverEvent);
+    while (!await new Promise(
+      resolve => setTimeout(() => {
+        let uiMenuInner = getUiMenuInner();
+        if (uiMenuInner instanceof Object) {
+          uiMenuInner.getElementsByTagName('a')[0].click();
+          resolve(true);
+        } else resolve(false);
+      }, 50)
+    )) {}
+    while (!await new Promise(
+      resolve => setTimeout(() => {
+        resolve(!(getUiMenuInner() instanceof Object));
+      }, 50)
+    )) {}
+  }
+
+  function getUiMenuInner() {
+    return document.getElementsByClassName('uiMenuInner')[3];
+  }
+}
+unlikePages();
